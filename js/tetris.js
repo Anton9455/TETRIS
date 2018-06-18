67dbf719e4f28107f8bb08c71045841b63a5e7a7
Anton9455 <anton445594@gmail.com>
2018-06-18 18:52:03 +0300

    Update

--- js/tetris.js
+++ js/tetris.js
@@ -8,50 +8,56 @@ tetris_context.fillRect(0, 0, tetris.width, tetris.height);
 const menu = document.getElementById('menu');
 const menu_context = menu.getContext('2d');
 menu_context.lineWidth = 3.5;
-menu_context.strokeRect(0,0,menu.width, menu.height);
-menu_context.scale(20,20);
+menu_context.strokeRect(0, 0, menu.width, menu.height);
+menu_context.scale(20, 20);
 
-class Element{
-    constructor(matrix){
+class Element {
+    constructor(matrix) {
         this.matrix = matrix;
     }
-    get_I() {
-        for(var i=0; i<this.matrix.length;i++){
-            for(var j=0; j<this.matrix[i].length;j++){
-                if(j == this.matrix.length-1){
-                    this.matrix[i][j] = 1;
-                    menu_context.fillStyle ='red';
-                    menu_context.fillRect(i + 0.5, //0.5 - отступ от левого края
-                                          j + 0.5,
-                                          1,1);
-                }
-            }
-        }
+    get_I(position, color, offset, orientation) {
+        arrayOperation(this.matrix, position, color, offset, orientation)
     }
 }
 
 const matrix = [
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0],
-    [0,0,0,0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
 ];
 var i_item = new Element(matrix);
-i_item.get_I();
+i_item.get_I(4, 'red', {
+             x: 0.5,
+             y: 0.5
+            },
+             true);
 
-function arrayOperation(matrix, position, color, offset){
-    for(var i=0; i<matrix.length;i++){
-        for(var j=0; j<matrix[i].length;j++){
-            if(i == position-1){
-                matrix[i][j] = 1;
-                menu_context.fillStyle =color;
-                menu_context.fillRect(i + offset.x,
-                                      j + offset.y,
-                                      1,1);
+function arrayOperation(matrix, position, color, offset, orientation) {
+    if (orientation == true) {
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                if (i == position - 1) {
+                    matrix[i][j] = 1;
+                    menu_context.fillStyle = color;
+                    menu_context.fillRect(i + offset.x,
+                        j + offset.y,
+                        1, 1);
+                }
             }
         }
+    } else {
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                if (j == position - 1) {
+                    matrix[i][j] = 1;
+                    menu_context.fillStyle = color;
+                    menu_context.fillRect(i + offset.x,
+                        j + offset.y,
+                        1, 1);
+                }
+            }
+        }
+
     }
 }
-
-
-
